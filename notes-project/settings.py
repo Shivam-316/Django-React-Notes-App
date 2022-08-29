7f165bf4a66e9d454f728655cf67983663abac3d
Shivammm <coolshiv77@gmail.com>
2022-08-29 14:11:53 +0530

    Heroku Config...

--- notes-project/settings.py
+++ notes-project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SK", default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default="False", cast='bool')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v : [s.strip() for s in v.split(',')])
 
 
 # Application definition
